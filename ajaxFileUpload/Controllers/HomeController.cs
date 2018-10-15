80c71560a02adebfb3caaf3cd7c62299dd12be80
bekir inan <bekir.inan@ogr.dpu.edu.tr>
2018-10-15 20:05:55 +0300

    initial commit

---
+++ ajaxFileUpload/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicFileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase[] files,int id)
+        {
+
+            for (int i = 0; i < files.Length; i++)
+            {
+
+                var folder = Server.MapPath("~/Upload");
+                var randomFileName = Path.GetRandomFileName();
+                var fileName = Path.ChangeExtension(randomFileName, ".jpg");
+                var path = Path.Combine(folder, fileName);
+
+
+
+
+                files[i].SaveAs(path);
+
+            }
+
+
+            return Json("");
+        }
+    }
+}
